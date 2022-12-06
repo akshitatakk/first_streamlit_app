38e80409e24eecc0f8b917bfefb8d905bcb61a60
akshitatakk <58398486+akshitatakk@users.noreply.github.com>
2022-12-06 18:37:55 +0530

    Update streamlit_app.py
    removing stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,8 +57,6 @@ if streamlit.button('Get Fruit List'):
   my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
- streamlit.stop()
-
 
 #allow end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
